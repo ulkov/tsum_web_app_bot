04acc902f306ec15b20d500fa235323e1875b5e8
Ульков Владимир Алексеевич <vaulkov@tsum.ru>
2025-02-04 08:03:03 +0300

    fix bot

--- bot.py
+++ bot.py
@@ -1,36 +1,43 @@
-import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command  # Используем фильтр для команд
-from aiogram.types import WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup, Message
+API_TOKEN = "7553842523:AAEGtGV8tsbyfMhALmbe2jEYMiTPkgGiSN0"
+web_app_url = "https://ulkov.github.io/tsum_web_app_bot/"
 
-API_TOKEN = '7553842523:AAEGtGV8tsbyfMhALmbe2jEYMiTPkgGiSN0'
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    WebAppInfo,
+    Message,
+    WebAppData,
+)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command("start"))
-async def send_welcome(message: Message):
-    web_app_url = "https://ulkov.github.io/tsum_web_app_bot/"
-    
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="Открыть Web App", web_app=WebAppInfo(url=web_app_url))]
-        ]
+
+@dp.message(F.text == "/start")
+async def start_command(message: Message):
+    web_app_button = KeyboardButton(
+        text="Open Mini App",
+        web_app=WebAppInfo(url=web_app_url),
+    )
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[[web_app_button]], resize_keyboard=True  # Note: List of lists
+    )
+    await message.answer(
+        "Press the button to open the Mini App:", reply_markup=keyboard
     )
 
-    await message.answer("Привет! Нажми кнопку ниже, чтобы открыть Web App:", reply_markup=keyboard)
 
-@dp.message(lambda message: message.web_app_data)
-async def web_app_data_handler(message: Message):
-    data = message.web_app_data.data  # Полученные данные из Web App
-    await message.answer(f"Получены данные: {data}")
+@dp.message(F.web_app_data)
+async def handle_web_app_data(message: Message):
+    web_app_data = message.web_app_data
+    if web_app_data:
+        await message.answer(f"Received data: {web_app_data.data}")
+    else:
+        await message.answer("No data received from Web App.")
 
-async def main():
-    try:
-        await bot.delete_webhook(drop_pending_updates=True)
-        await dp.start_polling(bot)
-    finally:
-        await bot.session.close()
 
-if __name__ == '__main__':
-    asyncio.run(main())
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(dp.start_polling(bot))
